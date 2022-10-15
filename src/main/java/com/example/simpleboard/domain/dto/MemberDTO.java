43abb37c0404a7e4e76f9f68942ee5cbc6fb3f25
soohykeee <gur1028@skuniv.ac.kr>
2022-10-15 22:15:23 +0900

    221015 swagger 적용 필요

---
+++ src/main/java/com/example/simpleboard/domain/dto/MemberDTO.java
@@ -0,0 +1,4 @@
+package com.example.simpleboard.domain.dto;
+
+public class MemberDTO {
+}
---
+++ src/main/java/com/example/simpleboard/infra/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.example.simpleboard.infra.config;
+
+//@Configuration
+public class SwaggerConfig {
+
+   /* @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }*/
+
+}
